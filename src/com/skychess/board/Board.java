a74d89e5179ac47106e9ebe25a3ebd267e5408e2
skylerputney <121919826+skylerputney@users.noreply.github.com>
2023-05-25 15:25:52 -0400

    More fleshing out of OOP

---
+++ src/com/skychess/board/Board.java
@@ -0,0 +1,13 @@
+package com.skychess.board;
+
+import java.awt.List;
+import com.skychess.pieces.Piece;
+
+public class Board {
+    private List<Piece> piecesOnBoard;
+    Date creationDate;
+
+    public Board(List<Tile> boardTiles) {
+        for(int i = 0; i < 64; i++)
+    }
+}
